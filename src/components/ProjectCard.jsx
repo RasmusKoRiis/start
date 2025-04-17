2f93bfce554c54f2f248097b3ae0e61847293cbe
RasmusKoRiis <rasmuskoriis@gmail.com>
2025-04-17 19:59:09 +0200

    Fices icon issue

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import ComputerIcon from "./icons/ComputerIcon";
 
 function ProjectCard({ name, url, icon, inlineSvg, tag }) {
-    return (
-      <a className="project" href={url} target="_blank" rel="noreferrer">
-        {inlineSvg === "computer" ? (
-          <ComputerIcon />
-        ) : (
-          <img src={icon} alt={name} />
-        )}
-        <div className="project-name">{name}</div>
-        {tag && <div className="project-tag">{tag}</div>}
-      </a>
-    );
-  }
+  const src = inlineSvg === "computer"
+    ? null
+    : `${process.env.PUBLIC_URL}/${icon}`;
+
+  return (
+    <a className="project" href={url} target="_blank" rel="noreferrer">
+      {inlineSvg === "computer" ? (
+        <ComputerIcon />
+      ) : (
+        <img src={src} alt={name} />
+      )}
+      <div className="project-name">{name}</div>
+      {tag && <div className="project-tag">{tag}</div>}
+    </a>
+  );
+}
+
   
 
 export default ProjectCard;
+
